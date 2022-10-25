305293e819af5873912cbeb1c81edad6f266654b
BT616 <110855674+BT616@users.noreply.github.com>
2022-10-24 19:26:51 -0700

    add friend path in user-controller

--- controller/user-controller.js
+++ controller/user-controller.js
@@ -2,7 +2,62 @@ const {User} = require('../models')
 
 const userController = {
 
- 
+   getUserById( req, res){
+    User.findById({_id: req.params.id})
+    .populate({path: ' thought', select: '-__v'})
+    .populate({ path: 'friends', select: '-__v'})
+    .select('-__v')
+    .then(userData =>{
+        if(!userData){
+res.status(404).json({message: 'no user with that id'})
+return;
+        }
+        res.json(userData)
+    }).catch(err => {
+        console.log(err);
+        res.status(400).json(err)
+    })
+   },
+   
+   updateUser({params, body}, res){
+    User.findOneAndUpdate({_id: params.id}, body, {new: true, runValidators: true})
+        .then(userData => {
+            if(!userData){
+                res.status(404).json({message: 'no user with that id'})
+                return;
+                        }
+                        res.json(userData);
+                    }).catch(err => {
+                        console.log(err);
+                        res.status(400).json(err)
+                    })   
+   },
+
+   deleteUser(req,ers){
+    User.findOneAndDelete({_id: params.id})
+    .then(userData => {
+        if(!userData){
+            res.status(404).json({message: 'no user with that id'})
+            return;
+                    }
+                    res.json(userData);
+                }).catch(err => {
+                    console.log(err);
+                    res.status(400).json(err)
+                })  
+
+   },
+    
+    //    
+    // 
+    // 
+    // addFriend,
+    // deleteFriend
+    addFriend(req,res){
+
+    },
+
+ //create
     createUser({body},res){
         User.create(body)
         .then((userData)=> 
@@ -10,6 +65,7 @@ const userController = {
         .catch((err) => 
        res.status(400).json(err))
     },
+    //get all
     getAllUser(req, res){
         User.find({})
          .populate({path: ' thought', select: '-__v'})
--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -3,11 +3,19 @@
 const router = require('express').Router();
 const { 
     getAllUser,
-    createUser 
+    createUser,
+    getUserById,
+    updateUser,
+    deleteUser,
+    addFriend,
+    deleteFriend
 } = require('../../controller/user-controller')
 
 
 router.route('/').get(getAllUser).post(createUser);
+router.route('/:id').get(getUserById).put(updateUser).delete(deleteUser);
+
+router.route('/:id/friends/:friendId').post(addFriend).delete(deleteFriend)
 
 
 module.exports = router;
